8a5326a666d50171f7ff95cc35c0b4bcd131d465
zqi4869 <145961948+zqi4869@users.noreply.github.com>
2024-09-03 21:40:16 +0800

    Add files via upload

---
+++ src/main/java/com/bookstore/domain/Book.java
@@ -0,0 +1,49 @@
+package com.bookstore.domain;
+
+public class Book {
+    private String title;
+    private String author;
+    private Integer year;
+    private String isbn;
+    private Double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
