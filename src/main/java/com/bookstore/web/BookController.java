6595bd0e9bb59a5b7052bd81ba0050abb5aa5c00
zqi4869 <145961948+zqi4869@users.noreply.github.com>
2024-09-13 08:48:14 +0800

    Add Listpage

--- src/main/java/com/bookstore/web/BookController.java
+++ src/main/java/com/bookstore/web/BookController.java
@@ -1,14 +1,24 @@
 package com.bookstore.web;
 
+import com.bookstore.domain.Book;
+import com.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookService bookService;
 
-    @GetMapping("/index")
-    public String index() {
-        return "hello";
+    @GetMapping("/booklist")
+    public String booklist(Model model) {
+        List<Book> bookList = bookService.findAllBooks();
+        model.addAttribute("booklist", bookList);
+        return "booklist";
     }
 
 }
