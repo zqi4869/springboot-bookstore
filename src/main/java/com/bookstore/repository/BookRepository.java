dfcce18f11e8d5182b2a257bd12244db819d4ebe
zqi4869 <145961948+zqi4869@users.noreply.github.com>
2024-09-18 13:31:19 +0800

    Enable Spring Data REST

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.bookstore.repository;
 
 import com.bookstore.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "book", path = "book")
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
--- src/main/java/com/bookstore/repository/CategoryRepository.java
+++ src/main/java/com/bookstore/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.bookstore.repository;
 
 import com.bookstore.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
